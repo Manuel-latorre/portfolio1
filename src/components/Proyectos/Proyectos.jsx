2609d3bfedde43f2e5a72d9852acae78e6a920de
Manuel-latorre <manuel.latorre11@gmail.com>
2023-08-22 15:43:47 -0300

    cambiandod etalles

--- src/components/Proyectos/Proyectos.jsx
+++ src/components/Proyectos/Proyectos.jsx
@@ -162,6 +162,149 @@ const Proyectos = () => {
             </Atropos>
 
         </div>
+
+
+    
+
+
+
+        <div className='containerProyectosMobile'>
+            
+                <div className='cardProyecto'>
+                            <img className='imgProyecto' src={radtek} alt="" data-atropos-offset="0" />
+                            <p style={{fontSize:30, fontWeight: 'bold', marginBottom: 20}}>Radtek</p>
+                            <p className='textCard'>Se trata Ecommerce de productos Gamers con el fin de simular el flujo de un Ecommerce real</p>
+                            <br />
+                            <hr />
+                            <br />
+                            <div className='sectionIcons'>
+                                <img className='icontech' src={ts} alt="Icono de Typescript" />
+                                <img className='icontech' src={react} alt="Icono de ReactJs" />
+                                <img className='icontech' src={css} alt="Icono de Css" />
+                                <img className='icontech' src={redux} alt="Icono de Redux" />
+                                <img className='icontech' src={ex} alt="Icono de Express" />
+                                <img className='icontech' src={postgresql} alt="Icono de PostgreSQL" />
+                                <img className='icontech' src={materialui} alt="Icono de Material UI" />
+                                <img className='icontech' src={bootstrap} alt='Icono de Bootstrap' />
+                            </div>
+                            <div className='containerBtn'>
+                                <a className='a' href="https://github.com/Lucas52352/Gaming-eCommerce" target='_blank'>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <GitHubIcon style={{marginRight: 10}} sx={{fontSize: 30}}/>
+                                    Visitar
+                                </a>
+                            </div>
+                      
+                    
+                </div>
+          
+
+
+        
+                <div className='cardProyecto'>
+                   
+                       
+                            <img className='imgProyecto' src={wise} alt="" />
+                            <p style={{fontSize:30, fontWeight: 'bold', marginBottom: 20}}>Wise Wallet</p>
+                            <p className='textCard'>Consiste en el desarrollo de un App para controlar tus gastos e ingresos.</p>
+                            <br />
+                            <hr />
+                            <br />
+                            <div className='sectionIcons'>
+                                <img className='icontech' src={ts} alt="Icono de Typescript" />
+                                <img className='icontech' src={react} alt="Icono de ReactJs" />
+                                <img className='icontech' src={css} alt="Icono de Css" />
+                                <img className='icontech' src={redux} alt="Icono de Redux" />
+                                <img className='icontech' src={ex} alt="Icono de Express" />
+                                <img className='icontech' src={postgresql} alt="Icono de PostgreSQL" />
+                                <img className='icontech' src={android} alt="Icono de Android" />
+                            </div>
+                            <div className='containerBtn'>
+                                <a className='a' href="https://github.com/Ezeko95/Wallet-Wise-client-app" target='_blank'>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <GitHubIcon style={{marginRight: 10}} sx={{fontSize: 30}}/>
+                                    Visitar
+                                </a>
+                            </div>
+                       
+                    
+                </div>
+           
+
+
+            
+                <div className='cardProyecto'>
+                    
+                        
+                            <img className='imgProyecto' src={countries} alt="" />
+                            <p style={{fontSize:30, fontWeight: 'bold', marginBottom: 20}}>MyCountry</p>
+                            <p className='textCard'>MyCountry es un sitio Web donde el usuario puede conocer distintos paises y sus actividades turisticas</p>
+                            <br />
+                            <hr />
+                            <br />
+                            <div className='sectionIcons'>
+                                <img className='icontech' src={js} alt="Icono de Javascript" />
+                                <img className='icontech' src={react} alt="Icono de ReactJs" />
+                                <img className='icontech' src={css} alt="Icono de Css" />
+                                <img className='icontech' src={redux} alt="Icono de Redux" />
+                                <img className='icontech' src={ex} alt="Icono de Express" />
+                                <img className='icontech' src={postgresql} alt="Icono de PostgreSQL" />
+                                
+                            </div>
+                            <div className='containerBtn'>
+                                <a className='a' href="https://github.com/Manuel-latorre/PI-Countries" target='_blank'>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <GitHubIcon style={{marginRight: 10}} sx={{fontSize: 30}}/>
+                                    Visitar
+                                </a>
+                            </div>
+                      
+                    
+                </div>
+        
+
+                <div className='cardProyecto'>
+                    
+                      
+                            <img className='imgProyecto' src={cactus} alt="" />
+                            <p style={{fontSize:30, fontWeight: 'bold', marginBottom: 20}}>Tienda Cactus</p>
+                            <p className='textCard'>Tienda Cactus es un E-commerce ficticio de streetwear. Proximamente me gustaria agregarle una pasarela de pagos para que sea mas real y dinamico.</p>
+                            <br />
+                            <hr />
+                            <br />
+                            <div className='sectionIcons'>
+                                <img className='icontech' src={html} alt="Icono de HTML" />
+                                <img className='icontech' src={css} alt="Icono de Css" />
+                                <img className='icontech' src={js} alt="Icono de Javascript" />
+                                <img className='icontech' src={bootstrap} alt="Icono de Bootstrap" />
+                                
+                            </div>
+                            <div className='containerBtn'>
+                                <a className='a' href="https://manuel-latorre.github.io/Tienda-Cactus/" target='_blank'>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                    <LanguageIcon style={{marginRight: 10}} sx={{fontSize: 30}}/>
+                                    Visitar
+                                </a>
+                            </div>
+
+                    
+                </div>
+            
+
+        </div>
+        
     </div>
   )
 }
